58a4d84099b4648f3eb236e3ec884887e63fac4e
yyhhoi <yyhhoi@gmail.com>
2023-06-02 14:33:31 +0200

    Rearrange

--- library/params.py
+++ library/params.py
@@ -325,8 +325,8 @@ class ParamsHolder:
 
     def fig5_NoRecurrence(self):
         config_dict = deepcopy(self.config_dict)
-        config_dict['Ipos_max'] = 5 # 6
-        config_dict['Iangle_diff'] = 9 # 8
+        config_dict['Ipos_max'] = 6 # 6
+        config_dict['Iangle_diff'] = 14 # 8
 
         config_dict['ECstf_rest'] = 0.25
         config_dict['ECstf_target'] = 1.5
@@ -337,7 +337,7 @@ class ParamsHolder:
         config_dict['wmax_ca3ca3_adiff'] = 0
         config_dict['w_ca3ca3_akappa'] = 1
         config_dict['asym_flag'] = False
-        config_dict['wmax_ca3mosca3_adiff'] = 5000
+        config_dict['wmax_ca3mosca3_adiff'] = 5500
         config_dict['mos_exist'] = True
         config_dict['wmax_CA3in'] = 50
         config_dict['wmax_inCA3'] = 5
@@ -348,8 +348,8 @@ class ParamsHolder:
 
     def fig6_NoRecurrence(self):
         config_dict = deepcopy(self.config_dict)
-        config_dict['Ipos_max'] = 5
-        config_dict['Iangle_diff'] = 9
+        config_dict['Ipos_max'] = 6 # 6
+        config_dict['Iangle_diff'] = 14 # 8
         config_dict['Iangle_compen'] = 2
         config_dict['ECstf_rest'] = 0.25
         config_dict['ECstf_target'] = 1.5
@@ -360,7 +360,7 @@ class ParamsHolder:
         config_dict['wmax_ca3ca3_adiff'] = 0
         config_dict['w_ca3ca3_akappa'] = 1
         config_dict['asym_flag'] = False
-        config_dict['wmax_ca3mosca3_adiff'] = 5000
+        config_dict['wmax_ca3mosca3_adiff'] = 5500
         config_dict['mos_exist'] = True
         config_dict['wmax_CA3in'] = 50
         config_dict['wmax_inCA3'] = 5
